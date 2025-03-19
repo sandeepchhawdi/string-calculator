e9b56e471ee0e9ef627857acdbe202fd3fab7773
Sandeep Kumar <sandeep.k@daffodilsw.com>
2025-03-19 22:55:39 +0530

    Added support of new line along with comma

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 class Calculator
   def add(str)
     sum = 0
-    int_arr = str.split(',')
+    int_arr = str.split(/[\n,]/)
     int_arr.each do |ele|
       sum += ele.to_i
     end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe Calculator do
     it 'passing 2,3 string, should return 5' do
       expect(calc.add("2,3")).to eq(5)
     end
+
+    it 'passing new line' do
+      expect(calc.add("1\n2,3")).to eq(6)
+    end
   end
+
+  context 'when passing invalid inputs' do
+    it 'should return zero' do
+      expect(calc.add("a,b")).to eq(0)
+    end
+  end
+
 end
