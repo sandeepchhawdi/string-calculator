659249e99b4684cc585b762a9050cbb81e601b31
Sandeep Kumar <sandeep.k@daffodilsw.com>
2025-03-19 23:16:34 +0530

    Added support of custom delimiter

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,14 @@
 class Calculator
   def add(str)
     sum = 0
-    int_arr = str.split(/[\n,]/)
+    delimiter = '[\n,]'
+
+    if str.start_with?("//")
+      delimiter, int_arr = str.split("\n", 2)
+      delimiter = delimiter.chars[2]
+    end
+
+    int_arr = str.split(/#{delimiter}/)
     int_arr.each do |ele|
       sum += ele.to_i
     end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -3,24 +3,31 @@ require_relative 'calculator'
 RSpec.describe Calculator do
   let(:calc) { Calculator.new }
 
-  context 'when passing valid inputs' do
-    it 'passing blank string, should return 0' do
-      expect(calc.add("")).to eq(0)
+  describe 'add' do
+    context 'using comma delimiter' do
+      it 'when passing blank string, should return 0' do
+        expect(calc.add("")).to eq(0)
+      end
+
+      it 'when passing non integer, should return zero' do
+        expect(calc.add("a,b")).to eq(0)
+      end
+
+      it 'when passing 2,3, should return 5' do
+        expect(calc.add("2,3")).to eq(5)
+      end
     end
 
-    it 'passing 2,3 string, should return 5' do
-      expect(calc.add("2,3")).to eq(5)
+    context 'using \n along with comma delimiter' do
+      it 'when passing \n along with comma' do
+        expect(calc.add("1\n2,3")).to eq(6)
+      end
     end
 
-    it 'passing new line' do
-      expect(calc.add("1\n2,3")).to eq(6)
+    context 'using custom delimiter' do
+      it 'when passing semicolon delimiter' do
+        expect(calc.add("//;\n1;2;3;4")).to eq(10)
+      end
     end
   end
-
-  context 'when passing invalid inputs' do
-    it 'should return zero' do
-      expect(calc.add("a,b")).to eq(0)
-    end
-  end
-
 end
