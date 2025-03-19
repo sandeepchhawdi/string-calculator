83bb6b7198ccdd4c49480246a26df0bd854b7b7e
Sandeep Kumar <sandeep.k@daffodilsw.com>
2025-03-19 23:31:50 +0530

    Added support of negative numbers

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,7 @@ class Calculator
   def add(str)
     sum = 0
     delimiter = '[\n,]'
+    negative_nums = []
 
     if str.start_with?("//")
       delimiter, int_arr = str.split("\n", 2)
@@ -10,8 +11,15 @@ class Calculator
 
     int_arr = str.split(/#{delimiter}/)
     int_arr.each do |ele|
-      sum += ele.to_i
+      ele = ele.to_i
+      if ele < 0
+        negative_nums.push(ele)
+        next
+      end
+      sum += ele
     end
+
+    raise "negative numbers not allowed: #{negative_nums.join(',')}" unless negative_nums.empty?
     sum
   end
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -16,17 +16,29 @@ RSpec.describe Calculator do
       it 'when passing 2,3, should return 5' do
         expect(calc.add("2,3")).to eq(5)
       end
+
+      it 'when passing negative numbers' do
+        expect { calc.add("-1,-2,3,-4") }.to raise_error("negative numbers not allowed: -1,-2,-4")
+      end
     end
 
     context 'using \n along with comma delimiter' do
       it 'when passing \n along with comma' do
         expect(calc.add("1\n2,3")).to eq(6)
       end
+
+      it 'when passing negative numbers' do
+        expect { calc.add("1,-2\n3,-4\n-5") }.to raise_error("negative numbers not allowed: -2,-4,-5")
+      end
     end
 
     context 'using custom delimiter' do
       it 'when passing semicolon delimiter' do
         expect(calc.add("//;\n1;2;3;4")).to eq(10)
+      end
+
+      it 'when passing negative numbers' do
+        expect { calc.add("//:\n-1:2:-3:4:-5") }.to raise_error("negative numbers not allowed: -1,-3,-5")
       end
     end
   end
